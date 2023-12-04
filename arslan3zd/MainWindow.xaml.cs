76e37b6b4b23d239f5df7667c4b8acd918a9ae74
pnbkiko <maxp3troff@yandex.ru>
2023-12-04 13:47:08 +0300

    Добавьте файлы проекта.

---
+++ arslan3zd/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Math;
+
+namespace arslan3zd
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(numa.Text);
+            double b = double.Parse(numb.Text);
+            double c = double.Parse(numc.Text);
+            double d = Pow(b, 2) - a * a * c;
+            double x1 = (-b + d) / (2 * a);
+            double x2 = (-b - d) / (2 * a);
+            otv.Text = $"Ответ: меньший x = {Min(x1, x2)}\n больший x = {Max(x1, x2)}";
+        }
+    }
+}
