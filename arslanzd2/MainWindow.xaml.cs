c113bf4d160dcc371a86e0e83bf77920c836ca65
pnbkiko <maxp3troff@yandex.ru>
2023-12-04 13:45:51 +0300

    Добавьте файлы проекта.

---
+++ arslanzd2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Math;
+
+namespace arslanzd2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double l = double.Parse(leni.Text);
+            const double pi = 3.14;
+            double r = l / (2 * pi);
+            double s = pi * Pow(r, 2);
+            otv.Text = $"Ответ: S = {s},\n R = {r}";
+        }
+    }
+}
